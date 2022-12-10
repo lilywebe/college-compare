244b7813e66e5cfbf0bfaad45dd0d7c56a4d5965
lilywebe <55253338+lilywebe@users.noreply.github.com>
2022-12-10 17:43:37 -0500

    change coloring of elements

--- src/index.js
+++ src/index.js
@@ -247,8 +247,8 @@ async function initUserColleges() {
         text: "You won't be able to revert this!",
         icon: "warning",
         showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
+        confirmButtonColor: "#900C3F",
+        cancelButtonColor: "#152238",
         confirmButtonText: "Yes, delete it!",
       }).then((result) => {
         if (result.isConfirmed) {
